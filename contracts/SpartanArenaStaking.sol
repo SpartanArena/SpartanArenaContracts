6d1adc15003787d27c188f80e88d63eeb0b1187c
Verifyfirst <verify.first@protonmail.com>
2021-12-20 20:06:50 +0930

    deposit&Withdraw

--- contracts/SpartanArenaStaking.sol
+++ contracts/SpartanArenaStaking.sol
@@ -1,18 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
+import "./TransferHelper.sol";
 
 contract SpartanArenaStaking {
     // ---------- Vars ----------
     address public spartaAddress; // SPARTA base contract address
-    address public chanceContract; // SPARTA base contract address
+    address public chanceContract; // Spartan arena chance contract address
     uint256 private globalStaked; // Total SPARTA staked globally
+
     // ---------- Maps ----------
     mapping(address => uint256) private mapMemberStake; // Total SPARTA staked by user
     mapping(address => uint256) private mapMemberTimestamp; // Timestamp user last staked
 
+    event MemberDeposits(
+        address indexed member,
+        uint256 amount
+    );
+     event MemberWithdraws(
+        address indexed member,
+        uint256 amount
+    );
+
     // ---------- Constructor ----------
-    constructor() {
-        // Assign 'PROTOCOL' role to 'Chance' contract (so it can reset member's stake time)
+    constructor(address base) {
+        spartaAddress = base;
     }
 
     // Restrict access (Use a role access library)
@@ -24,11 +35,29 @@ contract SpartanArenaStaking {
         mapMemberTimestamp[member] = block.timestamp + 60;
     }
 
+    function initChanceContract (address chanceAddress) external {
+          // Assign 'PROTOCOL' role to 'Chance' contract (so it can reset member's stake time)
+
+    }
+
     // ---------- Actions ----------
 
-    function deposit(uint256 amount) external {}
+    function deposit(uint256 amount) external {
+        require(amount > 0, '!VALID'); // Must be a valid amount
+        TransferHelper.safeTransferFrom(spartaAddress, msg.sender, address(this), amount);
+        mapMemberStake[msg.sender] += amount;
+        globalStaked += amount;
+        mapMemberTimestamp[msg.sender] = block.timestamp;
+        emit MemberDeposits(msg.sender, amount);
+    }
 
-    function withdraw(uint256 amount) external {}
+    function withdraw(uint256 amount) external {
+        require(amount >= mapMemberStake[msg.sender], "!STAKED");
+        mapMemberStake[msg.sender] -= amount;
+        globalStaked -= amount;
+        TransferHelper.safeTransfer(spartaAddress,  msg.sender,  amount);
+        emit MemberWithdraws(msg.sender, amount);
+    }
 
     // ---------- Getters ----------
 
